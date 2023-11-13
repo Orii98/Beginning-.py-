e9d221a866de42072b4db3b2a74608e7c9a231c0
Orii98 <mateusz.orecki95@gmail.com>
2023-11-13 23:38:04 +0100

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to football club name's generator")
+city = input("What's name of city that you grown up?\n")
+area = input("What's called your area in which you grown up ?\n")
+print("Yours FC name can be:" + area + ' ' + city)
